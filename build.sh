4a3b69e3146b720c6fb7a08bab5634b44389dd9e
xienng <741691740@qq.com>
2023-10-24 16:59:35 +0800

    修改shell文件

--- build.sh
+++ build.sh
@@ -2,32 +2,11 @@
 
 set -euo pipefail
 
-echo "Npm install"
-
+echo -e "\033[0;32mNpm install...\033[0m"
 npm install
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
 echo -e "\033[0;32mRunning hugo...\033[0m"
 
 hugo --minify --printPathWarnings --gc
 
-
-echo -e "\033[0;32m Uploading  pubilc folder...\033[0m"
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add -A
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin main
-
-# Come Back
-cd ..
