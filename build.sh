cb146ec408990a224679d38a15407428198f4f33
liujun2 <liujun2@sensetime.com>
2023-11-14 17:45:53 +0800

    增加子模块

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 set -euo pipefail
 
+echo -e "fetch submoudles"
+git submodule update --init --recursive
+
 echo -e "\033[0;32mNpm install...\033[0m"
 npm install
 
 
 echo -e "\033[0;32mRunning hugo...\033[0m"
+
 
 #
 rm -rf pubilc/*
---
+++ submodules.sh
@@ -0,0 +1,3 @@
+
+#添加书籍usage
+git submodule add git@github.com:geek-pie/docsy-usage.git content/usage
